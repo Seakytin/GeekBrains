e11b9b79cdd778e36b01868fb5cf6d8afe49b74a
Seakytin <Seakytin@yandex.ru>
2021-01-10 00:45:40 +0400

    Changed file from lesson 5 add 3 paragraph

--- src/homework/lesson5/Employee.java
+++ src/homework/lesson5/Employee.java
@@ -1,6 +1,8 @@
 package homework.lesson5;
 
+
 public class Employee {
+
     public String name;
     public String surname;
     public String patronymic;
@@ -20,8 +22,18 @@ public class Employee {
         this.salary = salary;
         this.age = age;
     }
-
     public static void main(String[] args) {
+        Employee person1 = new Employee("Sergei", "Akytin", "Aleksandrovich", "Designer", "Seakytin@yandex.ru", "89535513298", "45000", 27);
+        printInfo(person1);
+        Per();
+    }
+    private static void printInfo(Employee person1) {
+        System.out.printf("Person1 name: %s; surname: %s; patronymic: %s; position: %s; email: %s; number: %s; salary: %s; age: %d%n",
+                person1.name, person1.surname, person1.patronymic, person1.position, person1.email, person1.number, person1.salary, person1.age);
+
+    }
+
+    public static void Per() {
         Employee[] employees = new Employee[5];
         employees[0] = new Employee("Gosha", "Vasin", "Petrovich", "Cleaner", "Gosha@mail.ru", "89536698547", "48000", 44);
         employees[1] = new Employee("Gena", "Losin", "Ivanovich", "Manager", "Gena@mail.ru", "89536647296", "50000", 48);
@@ -33,7 +45,7 @@ public class Employee {
                 System.out.printf("name '%s'; surname: %s; patronymic: %s; position: %s; email: %s; number: %s; salary: %s; age: %d%n",
                         employees[i].name, employees[i].surname, employees[i].patronymic, employees[i].position, employees[i].email, employees[i].number, employees[i].salary, employees[i].age);
                  }
-            }
-        }
+           }
+       }
     }
 
